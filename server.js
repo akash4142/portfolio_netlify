b124c41aa847d038d0e4c635ca8f678879a04792
akash yadav <64635004+akash4142@users.noreply.github.com>
2024-02-25 11:32:13 -0500

    Add .gitignore

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -14,20 +14,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-=======
-const express = require('express');
-const path = require('path');
-const app = express();
-const PORT = 3000; // You can change this to your desired port number
 
-// Define a route
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'index.html'));
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
->>>>>>> 8b8069304c4a36ab6f5c908896102481c3462622
